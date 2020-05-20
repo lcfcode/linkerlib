bc05972746c9e8c2fa87835319855388ee9a46d5
liangcf <lcf@jionx.com>
2020-05-20 17:23:07 +0800

    重写验证码

--- utils/VerifyCode.php
+++ utils/VerifyCode.php
@@ -55,26 +55,38 @@ class VerifyCode
         if ($height == 0) {
             $height = floor($width / 3);
         }
+//        p($height);die;
         $img = imagecreate($width, $height);
         imagecolorallocate($img, 255, 255, 255);
-        for ($i = 0; $i < $width * 2; $i++) {
-            $pointColor = imagecolorallocate($img, mt_rand(0, 200), mt_rand(0, 200), mt_rand(0, 200));
-            imagesetpixel($img, mt_rand(0, $width), mt_rand(0, $height), $pointColor);
+        $pointNum = $width * 2;
+        if ($pointNum > 250) {
+            $pointNum = 250;
         }
+        $lineNum = $length;
+        if ($lineNum > 4) {
+            $lineNum = 4;
+        }
+        //随机颜色
+        $colorArr = [];
+        for ($i = 0; $i < $pointNum; $i++) {
+            $colorArr[] = imagecolorallocate($img, mt_rand(50, 200), mt_rand(50, 200), mt_rand(50, 200));
+        }
+        for ($i = 0; $i < $width; $i++) {
+            imagesetpixel($img, mt_rand(0, $width), mt_rand(0, $height), $colorArr[mt_rand(0, $pointNum - 1)]);
+            imagesetpixel($img, mt_rand(0, $width), mt_rand(0, $height), $colorArr[mt_rand(0, $pointNum - 1)]);
+        }
+        for ($i = 0; $i < $lineNum; $i++) {
+            imageline($img, mt_rand(0, ($width / 2) - $height), mt_rand(0, $height), mt_rand(($width / 2) + $height, $width), mt_rand(0, $height), $colorArr[mt_rand(0, $pointNum - 1)]);
+        }
+        for ($i = 0; $i < $lineNum - 1; $i++) {
+            imagearc($img, mt_rand(-10, $width + 10), mt_rand(-10, $height + 10), mt_rand($height, $width), mt_rand($height, $width), mt_rand(40, 50), mt_rand(30, 40), $colorArr[mt_rand(0, $pointNum - 1)]);
+        }
+
         for ($i = 0; $i < $length; $i++) {
-            $linkColor = imagecolorallocate($img, mt_rand(0, 220), mt_rand(0, 200), mt_rand(0, 200));
-            imageline($img, mt_rand(0, $width / 2), mt_rand(0, $height), mt_rand($width / 2, $width), mt_rand(0, $height), $linkColor);
-        }
-        for ($i = 0; $i < $length - 2; $i++) {
-            $arcColor = imagecolorallocate($img, mt_rand(0, 255), mt_rand(0, 200), mt_rand(0, 255));
-            imagearc($img, mt_rand(-10, $width + 10), mt_rand(-10, $height + 10), mt_rand($height, $width), mt_rand($height, $width), mt_rand(40, 50), mt_rand(30, 40), $arcColor);
-        }
-        for ($i = 0; $i < $length; $i++) {
-            $codeColor = imagecolorallocate($img, mt_rand(0, 200), mt_rand(0, 128), mt_rand(0, 200));
             $yRand = (int)$height / 3;
             $charX = (($i * $width) / $length) + mt_rand(($length - $i), $yRand);
-            $charY = mt_rand(3, $yRand + $length);
-            imagestring($img, 5, $charX, $charY, $code[$i], $codeColor);
+            $charY = mt_rand($yRand, $yRand + $length);
+            imagestring($img, 5, $charX, $charY, $code[$i], $colorArr[mt_rand(0, $pointNum - 1)]);
         }
         ob_clean();
         header('Cache-Control: private, max-age=0, no-store, no-cache, must-revalidate');
